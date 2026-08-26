cac160e4d75a77115bf642ba65a29c3884124cd5
caps-omarr <caps.omarr@gmail.com>
2026-08-26 17:12:11 +0800

    Fix 500 error on owner status toggle by adding missing BoardingHouse model import to AdminOwnerController

--- app/Http/Controllers/Admin/AdminOwnerController.php
+++ app/Http/Controllers/Admin/AdminOwnerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\ActivityLog;
+use App\Models\BoardingHouse;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
